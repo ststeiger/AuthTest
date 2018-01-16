de8d80ceb57f3c56441017abb49a36d6a180f4ee
ststeiger <quandary82@hailmail.net>
2018-01-16 17:36:23 +0100

    Account ctrl

--- AuthTest/Controllers/AccountController.cs
+++ AuthTest/Controllers/AccountController.cs
@@ -87,19 +87,58 @@ namespace AuthTest.Controllers
             System.Console.WriteLine(foo.MyText);
             // throw new Exception("foo");
 
-            this.m_MailService.SendMail("stefan.steiger@rsnweb.ch", "Stefan Steiger"
-                , "steiger@cor-management.ch", "Unauthorized", htmlTemplate
+            this.m_MailService.SendMail("stefan.steiger[at]rsnweb.ch", "Stefan Steiger"
+                , "steiger[at]cor-management.ch", "Unauthorized", htmlTemplate
             );
 
             return Content(htmlTemplate, "text/html");
         } // End Action NotAuthorized 
 
 
-        [Microsoft.AspNetCore.Authorization.AllowAnonymous]
-        //public async Task<ContentResult> SignIn(string ReturnUrl, string uname, string pwd)
-        public async Task<IActionResult> SignIn(string ReturnUrl, string uname, string pwd)
+
+        public class LoginViewModel
         {
-            System.Console.WriteLine(ReturnUrl);
+            
+            [System.ComponentModel.DataAnnotations.Required]
+            [System.ComponentModel.DataAnnotations.DataType(System.ComponentModel.DataAnnotations.DataType.Text)]
+            [System.ComponentModel.DataAnnotations.StringLength(50)]
+            [FromForm(Name = "uname")]
+            public string UserName { get; set; }
+
+            [System.ComponentModel.DataAnnotations.Required]
+            [System.ComponentModel.DataAnnotations.DataType(
+                System.ComponentModel.DataAnnotations.DataType.Password)
+            ]
+            [FromForm(Name ="pwd")]
+            public string Password { get; set; }
+
+
+            [System.ComponentModel.DataAnnotations.Display(Name = "Remember me?")]
+            // [System.ComponentModel.DataAnnotations.Required]
+            [FromForm(Name = "RememberMe")]
+            public bool RememberMe { get; set; }
+
+            [System.ComponentModel.DataAnnotations.DataType(
+                System.ComponentModel.DataAnnotations.DataType.Date)]
+            [System.ComponentModel.DataAnnotations.DisplayFormat(
+                DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+            [FromQuery(Name = "ed")]
+            public DateTime? EnrollmentDate { get; set; }
+        }
+
+
+        // [Microsoft.AspNetCore.Authorization.AllowAnonymous]
+        //[HttpPost, Microsoft.AspNetCore.Authorization.AllowAnonymous, ValidateAntiForgeryToken]
+        [HttpPost, Microsoft.AspNetCore.Authorization.AllowAnonymous]
+        //public async Task<ContentResult> SignIn(string ReturnUrl, string uname, string pwd)
+        //public async Task<IActionResult> SignIn(string ReturnUrl, string uname, string pwd)
+        public async Task<IActionResult> SignIn(string ReturnUrl, LoginViewModel loginData)
+        {
+            if (this.ModelState.IsValid)// && _userServices.ValidateUser(model.UserName, model.Password))
+            {
+                System.Console.WriteLine(loginData);
+                System.Console.WriteLine(ReturnUrl);
+            }
 
             //string htmlTemplate = blankHtmlTemplate;
             //htmlTemplate = htmlTemplate.Replace("@title", "Logged out");
@@ -110,24 +149,33 @@ namespace AuthTest.Controllers
             System.Console.WriteLine(remoteIpAddress);
 
 
-            List<System.Security.Claims.Claim> ls = new List<System.Security.Claims.Claim>();
+            // TODO: Check password...
 
-            ls.Add(
-                new System.Security.Claims.Claim(
-                    System.Security.Claims.ClaimTypes.Name, "IcanHazUsr_éèêëïàáâäåãæóòôöõõúùûüñçø_ÉÈÊËÏÀÁÂÄÅÃÆÓÒÔÖÕÕÚÙÛÜÑÇØ 你好，世界 Привет\tмир"
-                , System.Security.Claims.ClaimValueTypes.String
-                )
+
+            //List<System.Security.Claims.Claim> ls = new List<System.Security.Claims.Claim>();
+
+            //ls.Add(
+            //    new System.Security.Claims.Claim(
+            //        System.Security.Claims.ClaimTypes.Name, "IcanHazUsr_éèêëïàáâäåãæóòôöõõúùûüñçø_ÉÈÊËÏÀÁÂÄÅÃÆÓÒÔÖÕÕÚÙÛÜÑÇØ 你好，世界 Привет\tмир"
+            //    , System.Security.Claims.ClaimValueTypes.String
+            //    )
+            //);
+
+            //System.Security.Claims.ClaimsIdentity id = new System.Security.Claims.ClaimsIdentity("authenticationType");
+            //id.AddClaims(ls);
+
+            System.Security.Claims.ClaimsIdentity identity = 
+                new System.Security.Claims.ClaimsIdentity(Microsoft.AspNetCore.Authentication.Cookies
+                    .CookieAuthenticationDefaults.AuthenticationScheme);
+
+            identity.AddClaim(
+                new System.Security.Claims.Claim(System.Security.Claims.ClaimTypes.Name, loginData.UserName)
+                //new System.Security.Claims.Claim(System.Security.Claims.ClaimTypes.Name, uname)
             );
 
-            // 
-
-            System.Security.Claims.ClaimsIdentity id = new System.Security.Claims.ClaimsIdentity("authenticationType");
-            id.AddClaims(ls);
-
-            System.Security.Claims.ClaimsPrincipal principal = new System.Security.Claims.ClaimsPrincipal(id);
-
+            System.Security.Claims.ClaimsPrincipal principal = new System.Security.Claims.ClaimsPrincipal(identity);
+            AuthenticationProperties properties = new AuthenticationProperties { IsPersistent = true };
             // https://docs.asp.net/en/latest/security/authentication/cookie.html
-
 
             await HttpContext.SignInAsync(principal);
 
@@ -147,6 +195,19 @@ namespace AuthTest.Controllers
 
             string retUrl = System.Web.HttpUtility.HtmlAttributeEncode(System.Web.HttpUtility.UrlEncode(ReturnUrl));
 
+
+            // https://codepen.io/tag/login%20form
+            // https://codepen.io/design8383/pen/BKnFI?page=1&
+            // https://codepen.io/bbodine1/pen/fhLAg?page=2
+            // https://codepen.io/joshadamous/pen/yyyqJZ?page=9
+            // https://codepen.io/lexeckhart/pen/RPLPwX?page=12
+            // https://codepen.io/GrandvincentMarion/pen/epEPjp?page=14
+            // https://codepen.io/colorlib/pen/rxddKy?page=15
+            // https://codepen.io/andytran/pen/GpyKLM?page=16
+            // https://codepen.io/zacharyshupp/pen/YWwzqM?page=19
+            // https://codepen.io/dicson/pen/eJzoEX?page=21
+            // https://www.google.com/search?q=comparison+of+login+forms&hl=en&tbm=isch&tbo=u&source=univ&sa=X&ved=0ahUKEwiIjOycjtzYAhXRxqQKHWmCD-UQsAQIKA#imgrc=Y26FrkiqqUcGLM:
+            // https://www.google.com/search?hl=en&tbm=isch&sa=1&ei=-L1dWq3UDIS0kwXX5a-wCw&q=openid+logins&oq=openid+logins&gs_l=psy-ab.3...71771.73802.0.73937.13.13.0.0.0.0.72.697.13.13.0....0...1c.1.64.psy-ab..0.12.651...0j0i67k1j0i8i30k1j0i24k1j0i13k1j0i8i13i30k1.0.Bxblo71KARI#imgrc=erFOC8FEVhai6M:
             return Content(@"<!DOCTYPE html>
 <html>
 <head>
@@ -189,84 +250,96 @@ namespace AuthTest.Controllers
         }
 
 
-/* Bordered form */
-form {
-    border: 3px solid #f1f1f1;
-}
 
-/* Full-width inputs */
-input[type=text], input[type=password] {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    box-sizing: border-box;
-}
 
-/* Set a style for all buttons */
-button {
-    background-color: #4CAF50;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    cursor: pointer;
-    width: 100%;
-}
 
-/* Add a hover effect for buttons */
-button:hover {
-    opacity: 0.8;
-}
+        /* Bordered form */
+        form 
+        {
+            border: 3px solid #f1f1f1;
+        }
 
-/* Extra style for the cancel button (red) */
-.cancelbtn {
-    width: auto;
-    padding: 10px 18px;
-    background-color: #f44336;
-}
+        /* Full-width inputs */
+        input[type=text], input[type=password] 
+        {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            box-sizing: border-box;
+        }
 
-/* Center the avatar image inside this container */
-.imgcontainer {
-    text-align: center;
-    margin: 24px 0 12px 0;
-}
+        /* Set a style for all buttons */
+        button 
+        {
+            background-color: #4CAF50;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            cursor: pointer;
+            width: 100%;
+        }
 
-/* Avatar image */
-img.avatar {
-    width: 40%;
-    border-radius: 50%;
-}
+        /* Add a hover effect for buttons */
+        button:hover 
+        {
+            opacity: 0.8;
+        }
 
-/* Add padding to containers */
-.container {
-    padding: 16px;
-}
+        /* Extra style for the cancel button (red) */
+        .cancelbtn 
+        {
+            width: auto;
+            padding: 10px 18px;
+            background-color: #f44336;
+        }
 
-/* The ""Forgot password"" text */
-span.psw {
-                float: right;
-                padding - top: 16px;
+        /* Center the avatar image inside this container */
+        .imgcontainer 
+        {
+            text-align: center;
+            margin: 24px 0 12px 0;
+        }
+
+        /* Avatar image */
+        img.avatar 
+        {
+            width: 40%;
+            border-radius: 50%;
+        }
+
+        /* Add padding to containers */
+        .container 
+        {
+            padding: 16px;
+        }
+
+        /* The ""Forgot password"" text */
+        span.psw 
+        {
+            float: right;
+            padding - top: 16px;
+        }
+
+        /* Change styles for span and cancel button on extra small screens */
+        @media screen and(max - width: 300px) 
+        {
+            span.psw 
+            {
+                display: block;
+                float: none;
             }
-
-            /* Change styles for span and cancel button on extra small screens */
-            @media screen and(max - width: 300px) {
-                span.psw {
-                    display: block;
-                    float: none;
-                }
-    .cancelbtn {
-                    width: 100 %;
-                }
+    
+            .cancelbtn 
+            {
+                width: 100 %;
             }
+        }
 
     </style>
-
-    <script>
-
-    </script>
-
+    
 </head>
 <body>
     <h1>Login</h1>
@@ -300,17 +373,14 @@ span.psw {
 ", "text/html");
 
         } // End Action Login 
-
-
+        
 
         [Microsoft.AspNetCore.Authorization.AllowAnonymous]
-        public ContentResult Logout()
+        public async Task<IActionResult> Logout()
         {
-            string htmlTemplate = blankHtmlTemplate;
-            htmlTemplate = htmlTemplate.Replace("@title", "Logged out");
-            htmlTemplate = htmlTemplate.Replace("@body", "<h1>Logout</h1>");
-            
-            return Content(htmlTemplate, "text/html");
+            await HttpContext.SignOutAsync(Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return this.LocalRedirect("/");
         } // End Action Logout 
 
 
